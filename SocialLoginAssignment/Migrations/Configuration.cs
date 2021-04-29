fc450255d0031f6a5ba1047f7de352123b744e7a
ankitpraut@gmail.com <ankitpraut@gmail.com>
2021-04-29 12:51:32 +0530

    Importing project Details : Created project to showcase Social Login (Google,Facebook,Twitter and Linkedin) using ASP.Net MVC project with MySQL data (Code first approach).

---
+++ SocialLoginAssignment/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SocialLoginAssignment.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SocialLoginAssignment.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SocialLoginAssignment.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
